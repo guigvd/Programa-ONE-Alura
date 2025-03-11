94ddef8e667ef17ed676d1d53f7c17ac65048497
guigvd <contatoguigvd@hotmail.com>
2025-03-10 23:54:08 -0300

    add: funções de adicionar e sortear amigo

--- challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,28 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = []
+
+const erroMessage = () => {
+    return alert("Por favor, insira um nome!!")
+}
+
+// Função para adicionar amigos
+const adicionarAmigo = (nome) => {
+    nome = document.getElementById("amigo").value
+    if (nome === "") {
+        erroMessage()
+    } else {
+    amigos.push(nome)
+    }
+
+    document.getElementById("amigo").value = ""
+
+    console.log(amigos)
+}
+
+// Função para sortear amigo secreto
+const sortearAmigo = () => {
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)]
+    // document.getElementById("amigo-sorteado").innerHTML = amigoSorteado
+    console.log(amigoSorteado)
+}
